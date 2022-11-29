7be69a67abbb8f7d372dcd9c35a8f21570eb46aa
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2022-11-29 23:38:12 +0100

    License MPL2.0

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
-//
-// Created by lukasz on 25.11.2022.
-//
+/*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 #ifndef DFROBOT_SEN0251_UTILS_H
 #define DFROBOT_SEN0251_UTILS_H
