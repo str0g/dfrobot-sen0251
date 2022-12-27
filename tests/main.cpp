b4116eae64beb262dd864e8f16adcd1e4075c7a9
Łukasz Buśko <lukasz.busko@guns4hire.cc>
2022-12-27 12:34:29 +0100

    test app, runtime sea level pressure change

--- tests/main.cpp
+++ tests/main.cpp
@@ -37,7 +37,7 @@ int get_device_index() {
 
 struct Options {
   const char *description;
-  void (*fun)(Sen0251&);
+  void (*fun)(Sen0251&, char**, int&);
 };
 
 std::unordered_map<std::string, Options> actions;
@@ -57,11 +57,11 @@ void _help() {
   std::cout << std::endl;
 }
 
-void help(Sen0251& not_used) {
+void help(Sen0251& not_used, char **argv, int& argc) {
   _help();
 }
 
-void list_devices(Sen0251& not_used) {
+void list_devices(Sen0251& not_used, char **argv, int& argc) {
   auto rc = list_i2c_devices();
   std::cout << "possible devices: [ ";
   if (rc.empty()) {
@@ -73,34 +73,39 @@ void list_devices(Sen0251& not_used) {
   std::cout << "]" << std::endl;
 }
 
-void sensors(Sen0251& dev) {
+void sensors(Sen0251& dev, char **argv, int& argc) {
   for(int i=3; i; --i) {
     std::cout << dev.get_readings() << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 }
 
-void on(Sen0251& dev) {
+void on(Sen0251& dev, char **argv, int& argc) {
   dev.power_control();
 }
 
-void off(Sen0251& dev) {
+void off(Sen0251& dev, char **argv, int& argc) {
   dev.power_control(PowerControl::off);
 }
 
-void low_power(Sen0251& dev) {
+void low_power(Sen0251& dev, char **argv, int& argc) {
   dev.power_control(PowerControl::pressure_on | PowerControl::temperature_on | PowerControl::force_on_a);
 }
 
-void status(Sen0251& dev) {
+void status(Sen0251& dev, char **argv, int& argc) {
   dev.power_control();
   dev.set_oversampling(Oversampling::x8, Oversampling::x4);
   std::cout << dev << std::endl;
 }
 
-void misc(Sen0251& dev) {
+void misc(Sen0251& dev, char **argv, int& argc) {
   dev.get_status();
   dev.get_error();
+}
+
+void sea_level(Sen0251& dev, char **argv, int& argc) {
+  dev.sea_level_pressure_adjust(std::stof(argv[++argc]));
+  ++argc;
 }
 
 int main(int argc, char **argv) {
@@ -112,6 +117,7 @@ int main(int argc, char **argv) {
   actions["low_power"] = {"low_power", low_power};
   actions["status"] = {"status", status};
   actions["misc"] = {"misc", misc};
+  actions["sea"] = {"sea_level", sea_level};
 
   set_stdo_writer();
 
@@ -122,21 +128,22 @@ int main(int argc, char **argv) {
 
   if (argv[1] == std::string("list")) {
     Sen0251 *empty = nullptr;
-    list_devices(*empty);
+    list_devices(*empty, nullptr, argc);
     return 0;
   }
 
   Sen0251 dev(get_device_index());
-
-  for (int i = 1; i < argc; ++i) {
+  int i = 1;
+  while (i < argc) {
     try {
       auto &option = actions.at(argv[i]);
-      option.fun(dev);
+      option.fun(dev, argv, i);
     } catch (const std::out_of_range &e) {
       std::cout << "unknown " << argv[i] << std::endl;
       _help();
       return 0;
     }
+    ++i;
   }
 
   return 0;
